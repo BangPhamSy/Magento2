0250c38600cb2fdb40a8499707a352bc5a70487a
bangpham <bangphamsy97@gmail.com>
2018-04-11 13:29:44 +0700

    Push code project

---
+++ Magenest/Avatar/Block/Account/Dashboard/Info.php
@@ -0,0 +1,11 @@
+<?php
+namespace Magenest\Avatar\Block\Account\Dashboard;
+use Magento\Framework\App\Action\Context;
+
+class Info extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(Context $context)
+    {
+        parent::__construct($context);
+    }
+}
